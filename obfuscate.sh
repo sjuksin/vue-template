ec737d729a89154a96029970f71964594b9ea277
Dmitry Sjuksin <sjuksin@gmail.com>
2025-08-16 19:39:18 +0400

    update obfuscate.sh
    Улучшил вывод и обработку ошибок

--- obfuscate.sh
+++ obfuscate.sh
@@ -10,8 +10,9 @@ SRC_DIR="$1"
 OUT_DIR="$2"
 
 printf "Копируем из $SRC_DIR в $OUT_DIR и обфусцируем JS-файлы.\n"
-printf "НЕ копируем source-файлы (*.map.js)\n"
-printf "НЕ обфусцируем vendor-файлы"
+printf "При этом:\n"
+printf "  НЕ копируем source-файлы (*.js.map)\n"
+printf "  НЕ обфусцируем vendor-файлы"
 printf "\n--------\n\n"
 
 # Проверка, существует ли SRC_DIR
@@ -28,24 +29,29 @@ cp -r "$SRC_DIR/" "$OUT_DIR/"
 
 # Удаляем source-файлы
 find "$OUT_DIR" -type f -name "*.js.map" | while read file; do
-  echo "Удаляем source-файл: $file"
+  echo ">> Удаляем: $file (source-файл)"
   rm $file
 done
 
-# Обфусцируем все .js файлы внутри копии (кроме содержащих vendor)
-find "$OUT_DIR" -type f -name "*.js" | while read file; do
-  if [[ "$file" = *vendor* ]]; then
-     echo "Не обфусцируем vendor-файл: $file"
-     continue
-  else
-     echo "Обфусцируем $file"
-     javascript-obfuscator "$file" --output "$file"
+# Обфусцируем все .js файлы внутри копии (кроме файлов, содержащих в названии 'vendor')
+while IFS= read -r file; do
+  if [[ "$file" == *vendor* ]]; then
+    echo ">> Пропускаем: $file (vendor-файл)"
+    continue
   fi
-done
+
+  echo ">> Обфусцируем: $file"
+  # Обфусцуируем через библиотеку "javascript-obfuscator", игнорируем все сообщения кроме ошибок
+  if ! javascript-obfuscator "$file" --output "$file" >/dev/null; then
+    printf "\n❌ Ошибка при обфускации: $file.\n"
+    read -p "Нажмите Enter для выхода..." </dev/tty
+    exit 1
+  fi
+done < <(find "$OUT_DIR" -type f -name "*.js")
 
 printf "\n\n--------\n"
 printf "Обфускация кода завершена.\n"
-read -p "Нажмите Enter для завершения..."
+read -p "Нажмите Enter для завершения..." </dev/tty
 
 
 
