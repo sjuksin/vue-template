27ded8bf9c9db9d0be6c07f70ce89c49ef06b335
Dmitry Sjuksin <sjuksin@gmail.com>
2026-03-08 00:13:43 +0400

    chore: add postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import postcssPresetEnv from 'postcss-preset-env'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
+  css: {
+    postcss: {
+      plugins: [
+        postcssPresetEnv({ stage: 2 })
+      ]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
