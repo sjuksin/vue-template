d649079c482a932139971a19238abd7ae7c2a883
Dmitry Sjuksin <sjuksin@gmail.com>
2026-03-08 19:39:35 +0400

    fix: global stringify

--- src/helpers/setup-globals.ts
+++ src/helpers/setup-globals.ts
@@ -1,4 +1,3 @@
 import stringify from 'json-stringify-pretty-compact'
 
-// Компактный вывод объекта
-(globalThis as any).stringify = (data: any) => stringify(data).replace(/"(\w+)":/g, '$1:');
+(globalThis as any).stringify = (data: any) => data && stringify(data).replace(/"(\w+)":/g, '$1:')
