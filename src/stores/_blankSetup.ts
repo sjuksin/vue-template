161a4c7c52c721480006fe982f97c024c7c440a0
Dmitry Sjuksin <sjuksin@gmail.com>
2026-04-29 21:25:35 +0400

    feat: шаблоны для pinia - добавил setup-вариант

---
+++ src/stores/_blankSetup.ts
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+
+export const useXxxStore = defineStore('xxx', () => {
+  // --------------- State ---------------------
+
+  // --------------- Computed ------------------
+
+  // --------------- Actions -------------------
+
+  return {
+  }
+})
